71e4fa60756e5cc6be79c390d82cc16896bbe87e
onoja <onojamatthew59@gmail.com>
2021-04-25 01:40:28 +0100

    added new updates

--- client/src/components/Settings/Profile/Profile.js
+++ client/src/components/Settings/Profile/Profile.js
@@ -8,24 +8,28 @@ import "./Profile.css";
 const Profile = () => {
   return (
     <div id="setting-p" className="settings-profile-container">
-      <Row>
+      <Row  className="user-sidebar">
         <Col xs="12" xl="3">
           <Card className="profile-card">
             <CardBody>
-              <p className="text-center mt-4"> <Avatar size={100} /> </p>
+              <p className="text-center mt-4"> <Avatar size={100} /></p>
               <p className="text-center mt-4 p-name"> Onoja Matthew</p>
               <p className="text-center mt-4 p-ID">TMC/4563/JN</p>
               <p className="text-center mt-4 p-role">Admin</p>
-              <Row>
-                <Col xs="2" xl="2">
-                  <span>
-                    <DeleteOutlined />
+              <Row className="mt-5">
+                <Col xs="2" xl="2" className="pr-0 pt-2">
+                  <span className="icon-container">
+                    <DeleteOutlined className="delete-icon" />
                   </span>
                 </Col>
                 <Col xs="8" xl="8">
                   <Button className="active-button">Active</Button>
                 </Col>
-                <Col xs="2" xl="2"></Col>
+                <Col xs="2" xl="2" className="pl-0 pt-2">
+                  <span className="icon-container">
+                    <DeleteOutlined className="delete-icon" />
+                  </span>
+                </Col>
               </Row>
             </CardBody>
           </Card>
